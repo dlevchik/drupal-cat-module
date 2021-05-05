1e6126327aa637df6543151c1540d58daf097a3c
dlevchik <kanalqwesta@gmail.com>
2021-05-05 17:53:16 +0300

    form validation

--- web/modules/levchik/src/Form/CatsForm.php
+++ web/modules/levchik/src/Form/CatsForm.php
@@ -43,18 +43,21 @@ class CatsForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-//  public function validateForm(array &$form, FormStateInterface $form_state) {
-//    if (mb_strlen($form_state->getValue('message')) < 10) {
-//      $form_state->setErrorByName('name', $this->t('Message should be at least 10 characters.'));
-//    }
-//  }
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $form_state->clearErrors();
+    if (mb_strlen($form_state->getValue('catName')) < 2 || mb_strlen($form_state->getValue('catName')) > 32) {
+      $errText = $this->t('Cat\'s name should be at least 2 characters but less than 32 characters.');
+      $this->messenger()->addError($errText);
+      $form_state->setErrorByName('catName', $errText);
+    }
+  }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->messenger()->addStatus($this->t('The message has been sent.'));
-//    $form_state->setRedirect('<front>');
+    $form_state->setRedirect('levchik.cats');
   }
 
 }
